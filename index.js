e4d2795f5296464486e7041fe284b257ef9e300c
ramin keene <raminkeene@gmail.com>
2014-11-06 11:52:50 -0800

    move client lib to its own repo, naming potentially to change

---
+++ index.js
@@ -0,0 +1,62 @@
+(function(module){
+  'use strict';
+
+  var Q = require("q");
+
+  var Eventually = {
+    registry: {},
+    uuid: "",
+    elapsed: 0,
+    buckets: {},
+    customizations: {},
+    defaults: { timeout: 4000 },
+    _timeout: null,
+
+    timeout: function(time) {
+      this.customizations[timeout] = time;
+      return this;
+    },
+
+    startTimeoutCounter: function(deferred) {
+      var lock =  setTimeout(function() {
+        deferred.reject('timeout');
+      }, this.customizations.timeout || this.defaults.timeout);
+
+      this.customizations = {};
+      return lock;
+    },
+
+    expect: function(feature, doneCallback) {
+      if ( feature in this.buckets ) {
+
+        return Q.when(doneCallback(this.buckets[feature]));
+
+      } else {
+
+        var deferred = Q.defer();
+
+        deferred.promise.then(doneCallback);
+
+        this.registry[feature] = {
+          deferred: deferred,
+          timeout: this.startTimeoutCounter(deferred)
+        };
+        return this.registry[feature].deferred.promise;
+      }
+    },
+
+    receive: function(feature, value) {
+      if ( this.buckets[feature] == null ) {
+        this.buckets[feature] = value;
+      }
+
+      if ( feature in this.registry ) {
+        clearTimeout(this.registry[feature].timeout);
+        this.registry[feature].deferred.resolve(value);
+        delete this.registry[feature];
+      }
+    }
+  };
+
+  module.exports = Eventually;
+})(module);
